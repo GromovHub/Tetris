900dc812a89137c62f4873ea134faab68c7cdd57
Vitaly Gromov <gromovitaly@gmail.com>
2020-09-12 00:00:04 +0300

    102 str add game xml

--- app/src/main/java/create/tetris/MainActivity.kt
+++ app/src/main/java/create/tetris/MainActivity.kt
@@ -1,16 +1,56 @@
 package create.tetris
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import create.tetris.storage.AppPreferences
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
+
+    var tvHighScore: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
 
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScoreClick)
+        btnExit.setOnClickListener(this::onBtnExitClick)
+
+
+    }
+
+    private fun onBtnExitClick(view: View) {
+        exitProcess(0)
+    }
+
+    private fun onBtnResetScoreClick(view: View) {
+        val preferences = AppPreferences(this)
+        preferences.clearHighScore()
+        Snackbar.make(view, "Score successfully reset", Snackbar.LENGTH_SHORT).show()
+        tvHighScore?.text = "High score: ${preferences.getHighScore()}"
+
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+//        val preferences = AppPreferences(this)
+//        preferences.savedHighScore(2718)
+//        tvHighScore?.text = "High score: ${preferences.getHighScore()}"
     }
 
 
