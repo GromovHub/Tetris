6f1fcc63ff4afcff7610a8a843ff4789e5962324
Vitaly Gromov <gromovitaly@gmail.com>
2020-09-12 14:26:48 +0300

    110 str chapter 2 is end

--- app/src/test/java/create/tetris/ExampleUnitTest.kt
+++ app/src/test/java/create/tetris/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package create.tetris
 
+import create.tetris.storage.AppPreferences
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -14,4 +15,5 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 }
